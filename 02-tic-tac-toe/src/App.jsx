068a9fafcd58050fd6afa108fa8812525f264bd7
Cris160700 <arroyosemperecristian@gmail.com>
2026-02-27 19:23:46 +0100

    feat: initialize tic-tac-toe project with React and Vite
    - Add package.json with dependencies and scripts for development
    - Create main application structure with App component
    - Implement game logic including board state management and winner checking
    - Add Square and WinnerModal components for UI interaction
    - Style application with CSS for a better user experience
    - Include SVG assets for branding and icons
    - Configure Vite for React with SWC plugin

---
+++ 02-tic-tac-toe/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square.jsx'
+import { TURNS} from './constants'
+import { checkWinnerFrom } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
+import './App.css'
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) //Null es que no hay ganador, false es que hay un empate
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  //TODO: COMPONETIZAR TABLERO, REVISAR GANADOR
+  const updateBoard = (index) => {
+
+    /*No actualizamos esta posición
+    si ya tiene un valor*/
+    if (board[index] || winner) {
+      return
+    }
+
+    //Actualizar el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    //Cambiar el turno
+    const newTurn  = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    //Revisar si hay ganador
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      console.log('El ganador es: ' + newWinner)
+      console.log('El ganador es: ' + winner)
+      confetti()
+      setWinner(newWinner)
+    } else if (!newBoard.includes(null)) {
+      console.log('Empate')
+      setWinner(false) //Empate
+    }
+  }
+
+  console.log(board)
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <button onClick = {resetGame}>Reiniciar juego</button>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key = {index}
+                index = {index}
+                updateBoard={updateBoard}
+                >
+                  {board[index]}
+                </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected = {turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected = {turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+      <WinnerModal winner={winner} resetGame={resetGame} />
+    </main>
+  )
+}
+
+export default App
---
+++ 02-tic-tac-toe/src/components/WinnerModal.jsx
@@ -0,0 +1,28 @@
+import { Square } from './Square'
+export function WinnerModal({winner, resetGame}) {
+    if (winner === null) return null
+
+    const winnerText = winner === false ? 'Empate' : 'El ganador es: '
+    return (
+          <section className="winner">
+
+            <div className="text">
+              <h2>
+                { winnerText }
+              </h2>
+
+              <header className = "win">
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={() => {
+                  resetGame()
+                }}>
+                  Reiniciar juego
+                </button>
+              </footer>
+            </div>
+          </section>
+        )
+    }
